a10c84908466eb7890e15d790db96f96fb527e38
Erez Akdogan <erezakdogan.ea@gmail.com>
2021-09-21 22:57:54 +0300

    Database connection setup complete

--- src/Helpers/DBConnector.java
+++ src/Helpers/DBConnector.java
@@ -1,5 +1,27 @@
 package Helpers;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnector {
-    
+    private Connection connection;
+    private String url= "jdbc:postgresql://localhost/carrental";
+    private String user = "postgres";
+    private String password = "postgresql";
+
+    public Connection connectDB() {
+
+        try {
+            this.connection = DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return this.connection;
+    }
+
+    public static Connection getInstance(){
+        DBConnector dbConnector = new DBConnector();
+        return dbConnector.connectDB();
+    }
 }
