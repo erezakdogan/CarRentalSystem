a8515323db1aacf432997888307c2bf262007f04
Erez Akdogan <erezakdogan.ea@gmail.com>
2021-09-21 20:54:49 +0300

    Javafx and Postgresql librarys added

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
-public class App {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+    }
+
+    @Override
+    public void start(Stage arg0) throws Exception {
+        
     }
 }
