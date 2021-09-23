81a2c20fddbc3664dd231dbcaf7894036ffe5b7c
Erez Akdogan <erezakdogan.ea@gmail.com>
2021-09-23 15:34:33 +0300

    Customers RentsGUI controller created

--- src/View/CustomerGUI/RentsGUI/RentsGUI.java
+++ src/View/CustomerGUI/RentsGUI/RentsGUI.java
@@ -1,5 +1,24 @@
 package View.CustomerGUI.RentsGUI;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+
 public class RentsGUI {
-    
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private GridPane customerRentsGrid;
+
+    @FXML
+    void initialize() {
+        assert customerRentsGrid != null : "fx:id=\"customerRentsGrid\" was not injected: check your FXML file 'RentsGUI.fxml'.";
+
+    }
 }
